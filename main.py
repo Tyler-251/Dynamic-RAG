8022a66a323a8a1e46af2b69f665e8088be2bfdc
Tyler <tyler.wayne2002@hotmail.com>
2025-01-20 15:10:42 -0600

    reqs

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-import os
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+
